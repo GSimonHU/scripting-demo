a3486e7f7526df30209e43c82fb8f6f9501a1bb8
Gabor Simon <simon.gabor87hu@gmail.com>
2021-02-28 11:14:01 +0100

    Get CPU cores info

--- cpu-check.sh
+++ cpu-check.sh
@@ -6,9 +6,13 @@ COUNT=1
 while [ $NUMBER_OF_CPU_THREADS -ge $COUNT ]
 do
     echo "Info for CPU Thread $COUNT"
-    CPU_MODEL_NAME="$(grep 'model name' /proc/cpuinfo | cut -d$'\n' -f$COUNT | awk '{print $4 $5 $6 $7 $8$ 9}')"
-    echo "CPU Model: $CPU_MODEL_NAME"
+    CPU_MODEL="$(grep 'model name' /proc/cpuinfo | cut -d$'\n' -f$COUNT | awk '{print $4 $5 $6 $7 $8$ 9}')"
+    echo "CPU Model: $CPU_MODEL"
     
+    CPU_CORES="$(grep 'cpu cores' /proc/cpuinfo | cut -d$'\n' -f$COUNT | awk '{print $4}')"
+    echo "Number of CPU cores: $CPU_CORES"
+
+
     COUNT=`expr $COUNT + 1`
     echo
 done

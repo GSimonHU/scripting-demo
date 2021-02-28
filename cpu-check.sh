bf2cd56b484b168aae99db79fcd6c20a2a9cc2dc
Gabor Simon <simon.gabor87hu@gmail.com>
2021-02-28 11:03:27 +0100

    Get CPU model info

--- cpu-check.sh
+++ cpu-check.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-NUMBER_OF_CPU_THREADS=$(grep 'processor' /proc/cpuinfo | wc -l)
+NUMBER_OF_CPU_THREADS="$(grep 'processor' /proc/cpuinfo | wc -l)"
 echo $NUMBER_OF_CPU_THREADS
+
+COUNT=1
+while [ $NUMBER_OF_CPU_THREADS -ge $COUNT ]
+do
+    echo "Info for CPU Thread $COUNT"
+    CPU_MODEL_NAME="$(grep 'model name' /proc/cpuinfo | cut -d$'\n' -f$COUNT | awk '{print $4 $5 $6 $7 $8$ 9}')"
+    echo "CPU Model: $CPU_MODEL_NAME"
+    
+    COUNT=`expr $COUNT + 1`
+    echo
+done

91ec0d79507e89b8640159372e78d1707bcb3a7c
Gabor Simon <simon.gabor87hu@gmail.com>
2021-02-28 11:22:47 +0100

    Get CPU frequency

--- cpu-check.sh
+++ cpu-check.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 NUMBER_OF_CPU_THREADS="$(grep 'processor' /proc/cpuinfo | wc -l)"
-echo $NUMBER_OF_CPU_THREADS
 
 COUNT=1
 while [ $NUMBER_OF_CPU_THREADS -ge $COUNT ]
@@ -12,6 +11,9 @@ do
     CPU_CORES="$(grep 'cpu cores' /proc/cpuinfo | cut -d$'\n' -f$COUNT | awk '{print $4}')"
     echo "Number of CPU cores: $CPU_CORES"
 
+        
+    CPU_FREQUENCY="$(grep 'cpu MHz' /proc/cpuinfo | cut -d$'\n' -f$COUNT | awk '{print $4}')"
+    echo "CPUs frequency is: $CPU_FREQUENCY MHz"
 
     COUNT=`expr $COUNT + 1`
     echo

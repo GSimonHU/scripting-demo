f52c4eae3d2e8f91889759b6891f49c451d1997d
Gabor Simon <simon.gabor87hu@gmail.com>
2021-03-01 13:01:30 +0100

    Add formatting for memory info output

--- memory-check.sh
+++ memory-check.sh
@@ -1,2 +1,11 @@
 FREE_MEMORY="$(grep 'MemFree' /proc/meminfo | awk '{print $2}')"
 TOTAL_MEMORY="$(grep 'MemTotal' /proc/meminfo | awk '{print $2}')"
+
+FREE_MEMORY_IN_GB=$(($FREE_MEMORY/1000000))
+TOTAL_MEMORY_IN_GB=$(($TOTAL_MEMORY/1000000))
+FREE_TO_TOTAL_MEMORY=$((FREE_MEMORY*100/TOTAL_MEMORY))
+echo Memory Info
+echo Free memory: $FREE_MEMORY_IN_GB GB
+echo Total memory: $TOTAL_MEMORY_IN_GB GB
+echo "$FREE_TO_TOTAL_MEMORY%" of the memory is free
+echo

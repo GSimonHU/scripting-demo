8cf427739a120300688d4861bf92398441bfb281
Gabor Simon <simon.gabor87hu@gmail.com>
2021-03-03 08:23:16 +0100

    Get and display top 5 memory consuming processes

--- process-check.sh
+++ process-check.sh
@@ -1,9 +1,10 @@
 NUMBER_OF_TOP_PROCESSES=6
 #top 5, 6 because of header
+echo Top 5 processes consuming most memory
 
 IFS=$'\n'
 
-for tbl in $(ps -eo pid,%mem --sort=-%mem | head -n $NUMBER_OF_TOP_PROCESSES | grep -v 'PID')
+for tbl in $(ps -eo user,pid,%cpu,%mem --sort=-%mem | head -n $NUMBER_OF_TOP_PROCESSES)
 do
    echo $tbl
 done
